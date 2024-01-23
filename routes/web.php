09563a9d3d3873027bd620c72226567fc8743d8c
eman-m0hamed <emanmohamed73359@gmail.com>
2024-01-24 01:16:04 +0200

    add great price product route

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\ProductController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,3 +16,28 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+// product routes
+
+// get all products
+Route::get('/products', [ProductController::class, 'index'])->name('product.index');
+
+Route::get('/products/great', [ProductController::class, 'greatPrice'])->name('product.greatPrice');
+
+// add new product
+Route::post('/products', [ProductController::class, 'store'])->name('product.store');
+
+// add new product form
+Route::get('/products/create', [ProductController::class, 'create'])->name('product.create');
+
+// get product
+Route::get('/products/{id}', [ProductController::class, 'show'])->name('product.show');
+
+// delete product
+Route::delete('/products/{id}', [ProductController::class, 'destroy'])->name('product.destroy');
+
+// update product form
+Route::get('/products/{id}/update', [ProductController::class, 'update'])->name('product.update');
+
+// update product
+Route::put('/products/{id}', [ProductController::class, 'edit'])->name('product.edit');
+
