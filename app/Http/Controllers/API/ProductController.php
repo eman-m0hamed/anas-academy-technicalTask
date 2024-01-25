7fc840697cef26587a4adb8dcf4ed27987823dee
eman-m0hamed <emanmohamed73359@gmail.com>
2024-01-25 08:50:03 +0200

    create api product controller

---
+++ app/Http/Controllers/API/ProductController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Product;
+
+class ProductController extends Controller
+{
+    function index(Request $request)
+    {
+        
+    }
+}
