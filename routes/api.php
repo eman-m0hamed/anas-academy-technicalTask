e82febefefea78bd5d9d8bf0aa03e730df8a43c1
eman-m0hamed <emanmohamed73359@gmail.com>
2024-01-25 08:51:49 +0200

    add route to get products

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\API\ProductController;
+use App\Http\Controllers\APIProductController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -17,3 +18,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// get all products
+Route::get('products', [ProductController::class, 'index']);
+
