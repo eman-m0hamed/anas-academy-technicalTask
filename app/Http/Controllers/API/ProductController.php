c1e1666b2efbe8eda572bef5b2792fe8f2271bbd
eman-m0hamed <emanmohamed73359@gmail.com>
2024-01-25 08:50:56 +0200

    create index function to retrieve products data with pagination

--- app/Http/Controllers/API/ProductController.php
+++ app/Http/Controllers/API/ProductController.php
@@ -10,6 +10,29 @@ class ProductController extends Controller
 {
     function index(Request $request)
     {
-        
+        try {
+            $page = $request->input('page', 1);
+            $limit = $request->input('limit', 10);
+
+            $products = Product::paginate($limit, ['*'], 'page', $page);
+
+            $response = [
+                'success' => true,
+                'currentPage' => $products->currentPage(),
+                'perPage' => $products->perPage(),
+                'totalPages' => $products->lastPage(),
+                'totalProducts' => $products->total(),
+                'products' => $products->items(),
+                'message' => "Products data is Retrieved successfully",
+            ];
+
+            return response()->json($response, 200);
+        } catch (\Exception $error) {
+            $response = [
+                'success' => false,
+                'error' => "Server Error",
+            ];
+            return response()->json($response, 500);
+        }
     }
 }
