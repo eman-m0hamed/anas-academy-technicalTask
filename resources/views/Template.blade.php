7ff7cf873d2346dc896b20d6a8ea0f05542a7dea
eman-m0hamed <emanmohamed73359@gmail.com>
2024-01-25 11:08:19 +0200

    edit template

--- resources/views/Template.blade.php
+++ resources/views/Template.blade.php
@@ -9,29 +9,25 @@
     @yield('title')
     <!-- style -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    
+
     @yield('style')
 </head>
 
 <body>
 
     <main class="main">
-
-
-
-
         @yield('main')
-        @yield('script')
     </main>
 
     <!-- end contact section  -->
     {{-- <footer>
         <div class="copyright">
             Copyright ©2024 All rights reserved | This website is made by <a
-                href="#!">Eman Mohamed</a>
-        </div>
-    </footer> --}}
+            href="#!">Eman Mohamed</a>
+            </div>
+            </footer> --}}
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+@yield('script')
 </body>
 </html>
