dd3b2eada36dd65c99cbfbc44403b4e2b4b76aa5
eman-m0hamed <emanmohamed73359@gmail.com>
2024-01-25 12:59:55 +0200

    create project form using ajax instead of laravel form

--- resources/views/products/create.blade.php
+++ resources/views/products/create.blade.php
@@ -6,38 +6,32 @@
 
 @section('main')
     <section class="container">
-        <div class="row">
-            <h1 class="col-8 mt-5">Create New Product</h1>
-            <form action="{{ route('product.store') }}" method="post" class="col-6 mx-5">
+        <section class="row pt-5">
+            <h1 class="col-8" id="form-title">Create New Product</h1>
+
+            {{-- ajax form --}}
+            <form id="product-form" class="col-6 mx-5">
                 @csrf
+
+                {{-- product name field --}}
                 <div class="mt-3">
                     <label for="name" class="form-label">Product Name</label>
-                    <input type="text" class="form-control @error('name') is-invalid @enderror" id="name"
-                        name="name" value="{{ old('name') }}">
+                    <input type="text" class="form-control" id="name" name="name">
                 </div>
-                @error('name')
-                    <div class="text-danger">{{ $message }}*</div>
-                @enderror
 
+                {{-- product price field --}}
                 <div class="mt-3">
                     <label for="price" class="form-label">Product Price</label>
-                    <input type="text" class="form-control @error('price') is-invalid @enderror" id="price"
-                        name="price" value="{{ old('price') }}">
+                    <input type="text" class="form-control" id="price" name="price">
                 </div>
-                @error('price')
-                    <div class="text-danger">{{ $message }}*</div>
-                @enderror
 
+                {{-- product quantity field --}}
                 <div class="mt-3">
                     <label for="quantity" class="form-label">Product Quantity</label>
-                    <input type="number" class="form-control @error('quantity') is-invalid @enderror" id="quantity"
-                        name="quantity" value="{{ old('quantity') }}">
+                    <input type="number" class="form-control invalid" id="quantity" name="quantity">
                 </div>
-                @error('quantity')
-                    <div class="text-danger">{{ $message }}*</div>
-                @enderror
 
-
+                {{-- product category field --}}
                 <div class="mt-3">
                     <label for="category_id" class="form-label">Product Category</label>
                     <select name="category_id" id="category_id" class="form-select">
@@ -47,13 +41,70 @@
                         @endforeach
                     </select>
                 </div>
-                @error('category_id')
-                    <div class="text-danger">{{ $message }}*</div>
-                @enderror
 
-
+                {{-- submit button --}}
                 <button type="submit" class="btn btn-primary mt-3">Submit</button>
             </form>
-        </div>
+        </section>
+
     </section>
+@endsection
+
+@section('script')
+<!-- Include jQuery library -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+    <!-- AJAX script -->
+    <script>
+        $(document).ready(function() {
+            $('#product-form').submit(function(e) {
+                e.preventDefault(); // Prevent form submission
+
+                // Serialize the form data
+                let formData = $(this).serialize();
+
+                // remove errors from form
+                $('#product-form input').removeClass('border-danger');
+                $('#product-form select').removeClass('border-danger');
+                $('#product-form .error-message').remove();
+
+                // Send an AJAX request
+                $.ajax({
+                    url: "/api/products",
+                    type: "POST",
+                    data: formData,
+                    success: function(response) {
+                        console.log(response);
+
+                        // display success response message
+                        $('#form-title').before("<div class= 'alert alert-success'>" + response.message + "</div>");
+
+                        // reset the from
+                        $("#product-form"). reset();
+                    },
+
+                    error: function(error) {
+                        let response = error.responseJSON;
+                        console.log(response);
+                        if (response.errors && typeof(response.errors) === "object" && !Array.isArray(response.errors)) {
+
+                            // Iterate over the errors object and display them after each input
+                            $.each(response.errors, function(field, errors) {
+                                $('input[name="' + field + '"]').addClass('border-danger');
+                                $('select[name="' + field + '"]').addClass('border-danger');
+
+                                $('<div class="error-message text-danger">' + errors[0] + '*' + '</div>')
+                                    .insertAfter($('input[name="' + field + '"]'));
+
+                                $('<div class="error-message text-danger">' + errors[0] + '*' + '</div>')
+                                    .insertAfter($('select[name="' + field + '"]'));
+
+                            });
+                        }
+
+                    }
+                });
+            });
+        });
+    </script>
 @endsection
