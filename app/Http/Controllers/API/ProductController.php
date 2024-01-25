cfd8164935e65d111bc93421d20fc3e5480bb8d6
eman-m0hamed <emanmohamed73359@gmail.com>
2024-01-25 13:01:06 +0200

    add store function to api productController

--- app/Http/Controllers/API/ProductController.php
+++ app/Http/Controllers/API/ProductController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\API;
 use App\Http\Controllers\API\BaseController;
 use Illuminate\Http\Request;
 use App\Models\Product;
+use Illuminate\Support\Facades\Validator;
 
 class ProductController extends BaseController
 {
@@ -24,7 +25,29 @@ class ProductController extends BaseController
             ];
 
             return $this->sendResponse($products->items(), "Products data is Retrieved successfully", $details);
+        } catch (\Exception $error) {
+            return $this->sendError($error, "server error", 500);
+        }
+    }
 
+
+    function store(Request $request)
+    {
+        try {
+            $validator = Validator::make($request->all(), [
+                'name' => 'required|string',
+                'price' => 'required|numeric|regex:/^\d+(\.\d{1,2})?$/',
+                'quantity' => 'required|integer',
+                'category_id' => 'required|integer|exists:categories,id',
+
+            ]);
+            if ($validator->fails()) {
+                return $this->sendError($validator->errors(), "Please Validate Error", 400);
+            }
+
+            $input = $request->all();
+            $newProduct = Product::create($input);
+            return $this->sendResponse($newProduct, "The Product is Created Successfully");
         } catch (\Exception $error) {
             return $this->sendError($error, "server error", 500);
         }
